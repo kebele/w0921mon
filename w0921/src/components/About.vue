f1c45245a2b255537026d83a8ab93ba84eee5644
kebele <ykemalbeler@gmail.com>
2020-09-26 00:42:02 +0300

    ekleme

--- w0921/src/components/About.vue
+++ w0921/src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <!-- <v-container>
     <section class="skills section" id="certifications">
       <h2 class="section-title">certifications</h2>
 
@@ -48,6 +48,79 @@
         </div>
       </div>
     </section>
+  </v-container> -->
+  <v-container>
+    
+    <section>
+      <div class="slide">
+        <img
+            src="../assets/images/certifications/20_web_projects_kebele.jpg"
+            alt=""
+          />
+        <!-- <div class="content">
+          <h2>slide one</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, placeat.</p>         
+        </div> -->
+        </div>
+      <div class="slide">
+        <img
+            src="../assets/images/certifications/20_web_projects_kebele.jpg"
+            alt=""
+          />
+        <!-- <p>vanilla</p>
+        <div class="content">
+          <h2>slide two</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, placeat.</p>
+           
+        </div> -->
+        </div>
+      <div class="slide">
+        <img
+            src="../assets/images/certifications/vuejs_certificate_kebele.jpg"
+            alt=""
+          />
+        <!-- <div class="content">
+          <h2>slide three</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, placeat.</p>
+        </div> -->
+        </div>
+      <div class="slide">
+        <img
+            src="../assets/images/certifications/vuejs_certificate_kebele.jpg"
+            alt=""
+          />
+        <!-- <div class="content">
+          <h2>slide four</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, placeat.</p>
+        </div> -->
+        </div>
+      <div class="slide">
+        <img
+            src="../assets/images/certifications/vuejs_certificate_kebele.jpg"
+            alt=""
+          />
+        <!-- <div class="content">
+          <h2>slide five</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, placeat.</p>
+        </div> -->
+        </div>
+      <div class="slide">
+        <img
+            src="../assets/images/certifications/vuejs_certificate_kebele.jpg"
+            alt=""
+          />
+        <!-- <div class="content">
+          <h2>slide six</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, placeat.</p>
+        </div> -->
+      </div>
+    </section>
+    <section>
+     <div> <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat libero modi ea voluptatibus harum impedit enim deserunt omnis nesciunt praesentium, voluptatum commodi unde similique, aut eum voluptate? Nulla, harum explicabo.</h4>
+    </div>
+    </section>
+    
+      
   </v-container>
 </template>
 
@@ -57,7 +130,62 @@ export default {};
 
 <style scoped>
 img{
-    width : 100%;
-    height : auto;
+     width : 100%;
+    height : auto; 
+   
+}
+
+* {
+  margin: 0;padding: 0;box-sizing: border-box;
+}
+
+section {
+  position: relative;
+  width: 100%;
+  /* height: 100vh; */
+  display: flex;
+  border: 2px solid black;
+}
+
+section .slide{
+  position: relative;
+  background: green;
+  height: 100%;
+  flex : 1;
+  border-right: 2px solid black;
+  overflow: hidden;
+  transition: 0.5s ease-in-out;
+}
+section .slide:last-child{
+  border-right: none;
+}
+section .slide:hover{
+  flex-grow: 10;
+}
+/* section .slide:nth-child(1){
+  background-image: url(../assets/images/certifications/20_web_projects_kebele.jpg);
+  background-position: center center;
+  width: 100%;
+  height: auto;
+
+} */
+section .slide .content{
+  position: absolute;
+  bottom: 0;
+  margin: 40px;
+  padding: 30px;
+  background: black;
+  color: whitesmoke;
+  visibility: hidden;
+  opacity: 0;
+  transition: 0s;
+  transform: translateY(100px);
+}
+section .slide:hover .content{
+  visibility: visible;
+  opacity: 1;
+  transition: 0.5s;
+  transition-delay: 0.5s;
+  transform: translateY(0);
 }
 </style>
